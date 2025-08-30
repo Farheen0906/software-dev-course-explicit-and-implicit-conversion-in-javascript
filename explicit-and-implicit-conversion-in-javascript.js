a00ea4b3d201744747e2e93d91f8e2a4c60de56b
Farheen <farhashaik99@gmail.com>
2025-08-30 17:42:55 -0400

    Added Implicit and Explicit conversion examples to the code

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -15,10 +15,6 @@ Write their own code that demonstrates:
   *We encourage you to:
 Include at least one edge case, like NaN, undefined, or null .
 Use console.log() to clearly show the before-and-after type conversions.
-
-*/
-
-
 let result = "5" - 2;
 console.log("The result is: " + result);
 
@@ -27,6 +23,48 @@ if (isValid) {
     console.log("This is valid!");
 }
 
-let age = Number("25");
+let age = "25";
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+*/
+
+
+let result = "5" - 2; // Implicit conversion of string "5" to number and result is 3 as needed - so no change to the code
+console.log("The result is: " + result);
+
+let isValid = Boolean("false");//Explicit conversion and will evaluate to true 
+// so variable isvalid is assigned true , if block is executed here.
+if (isValid) {
+    console.log("This is valid!");
+}
+
+let age = Number("25");//Explicit conversion was necessary as totalAge variable was concatenation oftwo strings
+//  but the output needed was the addition of two numbers - so with use of Number() function explicit conversion is done result is 30
+let totalAge = age + 5;
+console.log("Total Age: " + totalAge);
+
+console.log("--------Implicit type conversion example--------");
+let inputNumber = 10;
+let inputString = "123";
+let additionOnInput = inputString + inputNumber ; //Implict coversion of 'num' variable to String type,the result is 12310
+console.log("The result is:" +additionOnInput);
+let subtractionOnInput = inputString - inputNumber; //Implict conversion of 'str' variable to Number type, result here is 113
+console.log("The result is:" +subtractionOnInput);
+console.log("--------Explicit type conversion example--------");
+let price = "300.99";
+let tax = 10;
+let totalAmount= Number(price) + tax; //Explict type conversion of price to Number type,total here is 310.99
+console.log ("Amount to be paid " +totalAmount);
+let userNo; //userNo is undefined -it can be classified as what are called falsy values.
+if(!userNo) //  if(!false) so the block of code is executed 
+{
+    console.log("Userno does not exist");
+}
+let studentId = null;
+if(studentId)
+{
+  console.log("student exists ");
+}else {
+  console.log("Student does not exists");
+}
